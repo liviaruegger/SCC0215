31d59ebcd717646487e0f8ac726896a868b6c1dd
Ana Lívia Ruegger Saldanha <analiviaruegger@gmail.com>
2022-05-23 18:16:48 -0300

    Update fixed.c

--- Trabalho01/fixed.c
+++ Trabalho01/fixed.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include "utils.h"
 
+#define HEADER_SIZE 182
+
 typedef struct register_type1
 {
     char removed;
@@ -85,7 +87,7 @@ reg_t1 *read_register_from_csv(FILE *fp)
     if (reg->city != NULL) reg->register_size += sizeof(reg->city - 1);
 
     if (reg->city != NULL) reg->city_namesize = sizeof(reg->city - 1);
-    else reg->city_namesize = -1;
+    else reg->city_namesize = 0;
 
     char *qtt = read_until(fp, ',');
     if (qtt[0] >= '0' && qtt[0] <= '9') reg->qtt = atoi(qtt);
@@ -104,7 +106,7 @@ reg_t1 *read_register_from_csv(FILE *fp)
     if (reg->brand != NULL) reg->register_size += sizeof(reg->brand - 1);
 
     if (reg->brand != NULL) reg->brand_namesize = sizeof(reg->brand - 1);
-    else reg->brand_namesize = -1;
+    else reg->brand_namesize = 0;
 
     char *model = read_until(fp, '\n');
     if (model[0] != '\0') reg->model = model;
@@ -112,32 +114,88 @@ reg_t1 *read_register_from_csv(FILE *fp)
     if (reg->model != NULL) reg->register_size += sizeof(reg->model - 1);
 
     if (reg->model != NULL) reg->model_namesize = sizeof(reg->model - 1);
-    else reg->city_namesize = -1;
+    else reg->city_namesize = 0;
 
     return reg;
 }
 
 void free_register(reg_t1 *reg)
-{
-    free(reg->state);
-    free(reg->city);
-    free(reg->brand);
-    free(reg->model);
-    free(reg);
+{   
+    if (reg)
+    {
+        if (reg->state) free(reg->state);
+        if (reg->city)  free(reg->city);
+        if (reg->brand) free(reg->brand);
+        if (reg->model) free(reg->model);
+        free(reg);
+    }
 }
 
 void read_and_write_register(FILE *input, FILE *output)
 {
     reg_t1 *reg = read_register_from_csv(input);
 
-    fwrite(reg->removed,       sizeof(char),     1, output);
-    fwrite(reg->register_size, sizeof(int),      1, output);
-    fwrite(reg->next,          sizeof(long int), 1, output);
-    fwrite(reg->id,            sizeof(int),      1, output);
-    fwrite(reg->year,          sizeof(int),      1, output);
-    fwrite(reg->qtt,           sizeof(int),      1, output);
+    // Campos de tamanho fixo
+    fwrite(reg->removed,        sizeof(char),     1, output); // 1
+    fwrite(&reg->register_size, sizeof(int),      1, output); // 4    5
+    fwrite(&reg->next,          sizeof(long int), 1, output); // 8   13
+    fwrite(&reg->id,            sizeof(int),      1, output); // 4   17
+    fwrite(&reg->year,          sizeof(int),      1, output); // 4   21
+    fwrite(&reg->qtt,           sizeof(int),      1, output); // 4   25
+    fwrite(&reg->qtt,           sizeof(int),      1, output); // 4   29
+    fwrite(reg->state,          sizeof(char),     2, output); // 2   31
+    
+    int bytes_written = 31;
 
-    // continuar
+    // Campos de tamanho variável
+    fwrite(&reg->city_namesize, sizeof(int),  1, output);
+    fwrite("0",                 sizeof(char), 1, output);
+    fwrite(reg->city,           sizeof(char), reg->city_namesize, output);
+    bytes_written += 4 + 1 + reg->city_namesize;
+
+    fwrite(&reg->brand_namesize, sizeof(int),  1, output);
+    fwrite("1",                  sizeof(char), 1, output);
+    fwrite(reg->brand,           sizeof(char), reg->brand_namesize, output);
+    bytes_written += 4 + 1 + reg->brand_namesize;
+
+    fwrite(&reg->model_namesize, sizeof(int),  1, output);
+    fwrite("2",                  sizeof(char), 1, output);
+    fwrite(reg->model,           sizeof(char), reg->model_namesize, output);
+    bytes_written += 4 + 1 + reg->model_namesize;
+
+    while (bytes_written < 97)
+    {
+        fwrite("$", sizeof(char), 1, output);
+        bytes_written++;
+    }
+    
+    // DEBUG
+    if (bytes_written > 97) printf("DEU PROBLEMA AQUI! ESTOUROU TAMANHO DO REGISTRO\n");
 
     free_register(reg);
+}
+
+void read_and_write_all(FILE *input, FILE *output)
+{   
+    char c = fgetc(input);
+    while (c != EOF)
+    {
+        ungetc(c, input);
+        read_and_write_register(input, output);
+    }
+}
+
+void close_file(FILE *fp)
+{
+    fseek(fp, 0, SEEK_SET);
+    fwrite("1", sizeof(char), 1, fp);
+
+    fseek(fp, 0, SEEK_END);
+    long int file_size = ftell(fp);
+    int next_rrn = ((file_size - HEADER_SIZE) / 97);
+
+    fseek(fp, 174, SEEK_SET);
+    fwrite(&next_rrn, sizeof(int), 1, fp);
+
+    fclose(fp);
 }
