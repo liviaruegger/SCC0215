fcd0ae350b8006076fa408f8137d6b784ab9a31c
Ana Lívia Ruegger Saldanha <analiviaruegger@gmail.com>
2022-05-24 19:37:59 -0300

    Update fixed.h

--- Trabalho01/fixed.h
+++ Trabalho01/fixed.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
-FILE *new_file(char *file_name);
-void read_and_write_all(FILE *input, FILE *output);
-void close_file(FILE *fp);
+FILE *new_type1_file(char *file_name);
+void read_and_write_all_type1(FILE *input, FILE *output);
+void close_type1_file(FILE *fp);
+void print_type1_register(FILE *fp, int rrn);
