e50ee4141cefa7222bcceca176e3666f603c8c4f
liviaruegger <analiviaruegger@gmail.com>
2022-07-14 19:32:10 -0300

    Create register struct

--- Trabalho03/data.c
+++ Trabalho03/data.c
@@ -15,6 +15,30 @@
 #include "utils.h"
 
 #define DATA_HEADER_SIZE_T1 182
-#define DATA_EADER_SIZE_T2 190
+#define DATA_HEADER_SIZE_T2 190
 
 #define REGISTER_SIZE_T1 97
+
+typedef struct reg
+{
+    char removed;
+    int  register_size; // Apenas tipo2
+    int  next;
+
+    int  id;
+    int  year;
+    int  qtt;
+    char *state;
+
+    int  city_namesize;
+    char *codeC5; // Apenas tipo2
+    char *city;
+
+    int  brand_namesize;
+    char *codeC6; // Apenas tipo2
+    char *brand;
+
+    int  model_namesize;
+    char *codeC7; // Apenas tipo2
+    char *model;
+}   reg_t;
