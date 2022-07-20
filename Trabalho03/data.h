ae8b62dd310de23a3ae0890ba855d14822deb48f
liviaruegger <analiviaruegger@gmail.com>
2022-07-20 20:03:03 -0300

    Update data.h

--- Trabalho03/data.h
+++ Trabalho03/data.h
@@ -17,8 +17,5 @@ void build_index_from_file_type2(FILE *data_fp, FILE *index_fp);
 void search_by_rrn_type1(FILE *fp, int rrn);
 void search_by_offset_type2(FILE *fp, long offset);
 
-void insert_registers_from_file_type1(FILE *data_fp, FILE *index_fp);
-void insert_registers_from_file_type2(FILE *data_fp, FILE *index_fp);
-
 void insert_new_registers_type1(FILE *data_fp, FILE *index_fp, int n_registers);
 void insert_new_registers_type2(FILE *data_fp, FILE *index_fp, int n_registers);
